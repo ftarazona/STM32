9bbc19473ba370451b94e746cfe8c3812aedd18a
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-09-26 19:10:21 +0200

    arm-asm-fill fill memory with pattern 'Hello World'

--- arm-asm-fill/deadbeef.s
+++ arm-asm-fill/deadbeef.s
@@ -1,8 +1,3 @@
-/* r0 - r3 addresses
-   r4 - r7 sizes
-   r8 - r11 patterns
-   */
-
 .syntax unified
 .arch armv7-m
 .cpu cortex-m4
@@ -11,44 +6,34 @@
 .global _start
 
 _start:
-    ldr r0, =0x10000000
-    ldr r1, =0x10000200
-    ldr r2, =0x10000500
-    ldr r3, =0x10000900
-    ldr r4, =0x200
-    ldr r5, =0x300
-    ldr r6, =0x400
-    ldr r7, =0x100
-    ldr r8, =0xc01dcafe
-    ldr r9, =0xdeadbeef
-    ldr r10, =0x00000000
-    ldr r11, =0xffffffff
-    ldr r12, =0x0
+	ldr r0, =0x10000200
+	ldr r1, =[mem]
 
 main:
-    bl fill
-    mov r0, r1
-    mov r4, r5
-    mov r8, r9
-    ldr r12, =0
-    bl fill
-    mov r0, r2
-    mov r4, r6
-    mov r8, r10
-    ldr r12, =0
-    bl fill
-    mov r0, r3
-    mov r4, r7
-    mov r8, r11
-    ldr r12, =0
-    bl fill
-    bl end
-    
-fill:
-    str r8, [r0, r12]
-    add r12, r12, #4
-    cmp r12, r4
-    bne fill
-    bx lr
+	bl strcpy
+	b end
 
-end: b end
+/* strcpy takes two arguments :
+    r0 is the destination address
+	r1 is the pattern address
+	it modifies both by increasing until r1 points to '\0'.
+	it uses r2 in order to store values temporarily.
+	it takes 
+*/
+strcpy:
+	ldr r2, [r1]
+	str r2, [r0]
+	add r0, r0, #4
+	add r1, r1, #4
+	and r2, r2, 0xff000000
+	cmp r2, #0
+	bne strcpy
+	bx lr
+
+end:
+	b end
+
+.balign 4
+mem:
+	.asciz "Hello World"
+	
