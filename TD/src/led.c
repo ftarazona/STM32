4d9f60915f52f7dd204a3f66696a130cd51f25b0
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-10-02 22:55:47 +0200

    Rewrote driver using CMSIS

--- TD/src/led.c
+++ TD/src/led.c
@@ -3,8 +3,8 @@
 /* led_init initializes PB14 in output mode */
 void led_init()	{
 	SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_GPIOBEN);
-	SET_BIT(RCC->AHB2ENR, RCC_GPIOCEN);
-	GPIOB_MODER = (GPIOB_MODER & 0xcfffffff) | GPIO_MODER14_OUTPUT; //Puts PB14 in output mode
+	SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_GPIOCEN);
+	GPIOB->MODER = (GPIOB->MODER & 0xcfffffff) | GPIO_MODER_MODER14_0; //Puts PB14 in output mode
 	led(LED_OFF);
 }
 
@@ -20,13 +20,13 @@ void led_g_off()	{
 
 void led(int state)	{
 	if(state == LED_OFF)	{
-		GPIOC_MODER = (GPIOC_MODER & 0xfff3ffff) | GPIO_MODER9_INPUT; //Puts PC9 in input mode (turns both LEDs off)
+		GPIOC->MODER = (GPIOC->MODER & 0xfff3ffff) | GPIO_MODER_MODER9; //Puts PC9 in input mode (turns both LEDs off)
 	} else	{
-		GPIOC_MODER = (GPIOC_MODER & 0xfff3ffff) | GPIO_MODER9_OUTPUT; //Puts PC9 in output mode (one LED on)
+		GPIOC->MODER = (GPIOC->MODER & 0xfff3ffff) | GPIO_MODER_MODER9_0; //Puts PC9 in output mode (one LED on)
 		if(state == LED_YELLOW)	{
-			GPIOC_BSRR |= GPIO_BSRR9_SET;
+			SET_BIT(GPIOC->BSRR, GPIO_BSRR_BS9);
 		} else	{
-			GPIOC_BSRR |= GPIO_BSRR9_RESET;
+			SET_BIT(GPIOC->BSRR, GPIO_BSRR_BR9);
 		}
 	}
 }
