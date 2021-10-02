28cf2f629d614e55c895145c2da862e8107da9a9
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-10-02 23:50:16 +0200

    WIP

---
+++ TD/inc/uart.h
@@ -0,0 +1,9 @@
+#ifndef UART_H
+#define UART_H
+
+#include "stm32l4xx.h"
+#include "stm32l475xx.h"
+
+void uart_init();
+
+#endif
---
+++ TD/src/uart.c
@@ -0,0 +1,6 @@
+#include "uart.h"
+
+/* uart_init initializes uart1 to establish a serial communication. */
+void uart_init()	{
+
+}
