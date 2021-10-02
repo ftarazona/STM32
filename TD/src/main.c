09aa1774c79be07ada3e78211e306a8284ab4244
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-10-02 22:12:49 +0200

    Added CMSIS headers

--- TD/src/main.c
+++ TD/src/main.c
@@ -1,12 +1,13 @@
 #include "led.h"
 #include "clocks.h"
+#include "stm32l475xx.h"
 
 #define WAITING_TIME 1000000
 
 int main(void)	{
 	clocks_init();
 	led_init();
-
+	
 	while(1)	{
 		led(LED_OFF);
 		led_g_on();
