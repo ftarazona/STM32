23c016c93e9ea555324e8a3b00344b97120ad412
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-10-02 18:59:52 +0200

    WIP

---
+++ TD/inc/led.h
@@ -0,0 +1,12 @@
+#ifndef LED_H
+#define LED_H
+
+#define RCC_AHB2ENR	(*(volatile uint32_t *)0x4002104c)
+#define GPIOB_MODER	(*(volatile uint32_t *)0x48000400)
+#define GPIOB_BSRR	(*(volatile uint32_t *)0x48000418)
+
+#define GPIO_MODER14_OUTPUT 	0b01 << 28
+#define GPIO_BSRR14_RESET		0b1 << 30
+#define GPIO_BSRR14_SET			0b1 << 14
+
+#endif
