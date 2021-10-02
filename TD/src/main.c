7ce56fbc58d76560ba268cba4df3080dd34ac795
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-10-02 21:19:54 +0200

    Green-Yellow-Blue LEDs blink

--- TD/src/main.c
+++ TD/src/main.c
@@ -6,12 +6,19 @@ int main(void)	{
 	led_init();
 
 	while(1)	{
+		led(LED_OFF);
 		led_g_on();
 
 		for(int i = 0; i < WAITING_TIME; ++i)
 			asm volatile("nop");
 
 		led_g_off();
+		led(LED_YELLOW);
+
+		for(int i = 0; i < WAITING_TIME; ++i)
+			asm volatile("nop");
+
+		led(LED_BLUE);
 
 		for(int i = 0; i < WAITING_TIME; ++i)
 			asm volatile("nop");
