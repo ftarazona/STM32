c1f9f34982e857f3fc44869a6ef44fe9cf226bac
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-10-03 02:11:44 +0200

    WIP

--- TD/src/main.c
+++ TD/src/main.c
@@ -1,29 +1,13 @@
 #include "led.h"
 #include "clocks.h"
+#include "uart.h"
 #include "stm32l475xx.h"
 
 #define WAITING_TIME 1000000
 
 int main(void)	{
 	clocks_init();
-	led_init();
-	
-	while(1)	{
-		led(LED_OFF);
-		led_g_on();
+	uart_init();
 
-		for(int i = 0; i < WAITING_TIME; ++i)
-			asm volatile("nop");
-
-		led_g_off();
-		led(LED_YELLOW);
-
-		for(int i = 0; i < WAITING_TIME; ++i)
-			asm volatile("nop");
-
-		led(LED_BLUE);
-
-		for(int i = 0; i < WAITING_TIME; ++i)
-			asm volatile("nop");
-	}
+	uart_putchar('F');
 }
