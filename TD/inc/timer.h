cadc6b7fb25d08113bf90cc67d58fa4d396bd487
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-10-07 23:57:04 +0200

    WIP

--- TD/inc/timer.h
+++ TD/inc/timer.h
@@ -1,8 +1,10 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include "stm32l4xx.h"
 #include "stm32l475xx.h"
 
 void timer_init(int max_us);
+void TIM2_IRQHandler(void);
 
 #endif
--- TD/src/timer.c
+++ TD/src/timer.c
@@ -3,9 +3,18 @@
 /* timer init initializes the timer TIM2 and links a IRQ */
 void timer_init(int max_us)	{
 	//Enabling TIM2 on APB1
-	SET_BIT(RCC->APB1ENR, RCC_APB1ENR_TIM2EN);
+	SET_BIT(RCC->APB1ENR1, RCC_APB1ENR1_TIM2EN);
 
 	//Reseting the timer
 	TIM2->CNT = 0;
 	TIM2->PSC = 80;
+	TIM2->ARR = max_us << 16;
+	SET_BIT(TIM2->DIER, TIM_DIER_TIE);
+
+	//Enabling the IRQ in NVIC
+	NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void TIM2_IRQHandler(void)	{
+	CLEAR_BIT(TIM2->SR, TIM_SR_TIF);
 }
