230d0dca71689fb6d2d5d30d1156aabbd09b773e
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-10-10 01:59:15 +0200

    WIP

--- TD/inc/matrix.h
+++ TD/inc/matrix.h
@@ -6,6 +6,10 @@
 
 #include "timer.h"
 
+#define LED_MATRIX_WIDTH 8
+#define LED_MATRIX_HEIGHT 8
+#define LED_MATRIX_N_LEDS LED_MATRIX_WIDTH * LED_MATRIX_HEIGHT
+
 #define N_TICKS_DELAY 100000
 
 #define SB(x)	if(x) {SET_BIT(GPIOC->BSRR, GPIO_BSRR_BS5);} else {SET_BIT(GPIOC->BSRR, GPIO_BSRR_BR5);}
@@ -25,17 +29,33 @@
 #define pulse_SCK SCK(1); SCK(0);
 #define pulse_LAT LAT(0); LAT(1);
 
+/* rgb_color describes the state of 3-color led */
 typedef struct	{
 	uint8_t r;
 	uint8_t g;
 	uint8_t b;
 } rgb_color;
 
+rgb_color led_values[LED_MATRIX_N_LEDS];
+
+/* Initialization. MUST BE CALLED BEFORE ANY LED MATRIX MANIPULATION
+ * */
 void matrix_init(void);
+
+/* Deactivates every row */
 void deactivate_rows(void);
+/* Activates a given row */
 void activate_row(int row);
+
+/* Sends 8 bit to the matrix. THIS FUNCTION MAY NOT BE CALLED BY 
+ * USER */
 void send_byte(uint8_t val, int bank);
+/* Sets BANK0. THIS FUNCITON MAY NOT BE CALLED BY USER. */
+void init_bank0(void);
+
+/* sets a row to given values */
 void mat_set_row(int row, const rgb_color * val);
-void init_bank0();
+/* displays image described by led_values array */
+void display_image(void);
 
 #endif
