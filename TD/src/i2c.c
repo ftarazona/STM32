a134afca1116d7df99a791b011dfcff3c3354388
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-10-25 13:30:26 +0200

    WIP

--- TD/src/i2c.c
+++ TD/src/i2c.c
@@ -4,6 +4,12 @@ void i2c_master_init(uint8_t saddr)	{
 	//Enable clock for I2C
 	SET_BIT(RCC->APB1ENR1, RCC_APB1ENR1_I2C1EN);
 
+	//Setting frequency to 400kHz
+	I2C1->I2C_TIMINGR |= 	(200 << I2C_TIMINGR_SCLL_Pos) |
+							(200 << I2C_TIMINGR_SCLH_Pos);
+
+	//Enabling I2C
+	SET_BIT(I2C1->I2C_CR1, I2C_CR1_PE);
 }
 
 void i2c_send(uint8_t data, uint8_t subaddr)	{
